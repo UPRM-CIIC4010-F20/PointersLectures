20bfdf4eaf62ac90f91f04ac00802f588342b1ed
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2020-10-18 18:16:32 -0400

    Minor changes

--- pointers.cpp
+++ pointers.cpp
@@ -8,28 +8,20 @@ class Node {
     string label;
     Node* next;
     Node(string label, Node* next): label(label), next(next) { }
-    Node(): label(""), next(nullptr) { }
 };
 
-static Node node;
-static Node* nodePtr = nullptr;
+static Node node("Global Node", nullptr);
+static Node* nodePtr = &node;
 
 static int f() {
-    nodePtr = new Node();
-    nodePtr->label = "f() Node";
+    nodePtr = new Node("f() Node",nullptr);
+    // nodePtr->label = new string("Local Node");
+
     return 0;
 }
 
 int main() {
-    // while (true) {
-        node.label = "Global Node";
-        node.next = nullptr;
-        nodePtr = &node;
+    //  while (true) {
         f();
-        cout << "node.label = " << node.label << endl;
-        cout << "&node = " << &node << endl;
-        cout << "nodePtr = " << nodePtr << endl;
-        cout << "nodePtr->label = " << nodePtr->label << endl;
-    // }
-    
+    //  } 
 }
