faaa0f53a1fc9a6ecb8f1af279568c04b4b73637
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2020-10-18 16:12:25 -0400

    Initial Commit

---
+++ pointers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Node {
+    public:
+    string label;
+    Node* next;
+    Node(string label, Node* next): label(label), next(next) { }
+    Node(): label(""), next(nullptr) { }
+};
+
+static Node node;
+static Node* nodePtr = nullptr;
+
+static int f() {
+    nodePtr = new Node();
+    nodePtr->label = "f() Node";
+    return 0;
+}
+
+int main() {
+    // while (true) {
+        node.label = "Global Node";
+        node.next = nullptr;
+        nodePtr = &node;
+        f();
+        cout << "node.label = " << node.label << endl;
+        cout << "&node = " << &node << endl;
+        cout << "nodePtr = " << nodePtr << endl;
+        cout << "nodePtr->label = " << nodePtr->label << endl;
+    // }
+    
+}
+
+s
